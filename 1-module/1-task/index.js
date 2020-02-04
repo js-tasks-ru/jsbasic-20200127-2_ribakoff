652de9a2ebd4e4d67ef8682fe9510a4f56ef7b29
Kirill Rybakov <kirill.fisherov@gmail.com>
2020-02-04 23:07:29 +0700

    Написал функцию factorial в 1й задаче раздела 1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -3,6 +3,12 @@
  * @param {number} n
  * @returns {number}
  */
-function factorial(n) {
-
-}
+ function factorial(n) {
+   if ( n === 0){
+     return n = 1;
+   }
+   for ( let minus = n - 1; minus > 0; minus-- ){
+     n *= minus;
+   }
+   return n;
+ }
