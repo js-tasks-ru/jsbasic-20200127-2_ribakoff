3db88374d9dec6a7939a74d8a2406a4b57e0070a
Kirill Rybakov <kirill.fisherov@gmail.com>
2020-02-12 18:26:52 +0700

    Написал функцию filterRange для 5oй задачи из раздела 3

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -5,4 +5,9 @@
  * @returns {number[]}
  */
 function filterRange(arr, a, b) {
+  let filterArray = [];
+  arr.forEach((item) => {
+    if (item >= a && item <= b) filterArray.push(item)
+  });
+  return filterArray;
 }
